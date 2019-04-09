9fe09735d4ea0c78e00e48b993f83028119e9535
antonellam <antonella.michelli@gmail.com>
2019-04-09 23:42:28 +0200

    Add function to change text

--- js/main.js
+++ js/main.js
@@ -1,38 +1,40 @@
 'use strict';
 
-console.log('Ready to go');
-
 const randomNumber = randomNum(100);
 const numberEl = document.querySelector('.number__input');
 const buttonEl = document.querySelector('.number__submit');
 const clueEl =  document.querySelector('.clue');
 const contEl = document.querySelector('.counter');
-console.log(numberEl, buttonEl, clueEl, contEl);
-
 let counter = 0;
+
+console.log(randomNumber);
 
 function randomNum() {
     return Math.ceil(Math.random() * 100);
 }
 
+function clueText(text) {
+    clueEl.innerHTML = text;
+}
+
 function checkNumber () {
     let comparedNumber = parseInt(numberEl.value);
     if (comparedNumber === randomNumber) {
-        clueEl.innerHTML = "¡HAS GANADO, CAMPEONA!"
+        clueText("¡HAS GANADO, CAMPEONA!");
     } else if (comparedNumber > randomNumber) {
-        clueEl.innerHTML = "Prueba un número más bajo"
+        clueText("Prueba un número más bajo");
     } else if (comparedNumber < randomNumber) {
-        clueEl.innerHTML = "Prueba un número más alto";
+        clueText("Prueba un número más alto");
     } else {
-        clueEl.innerHTML = "Escribe un número y dale a prueba";
+        clueText("Escribe un número y dale a prueba");
     }
 }
 
 function buttonClickHandler(event) {
     event.preventDefault();
-    checkNumber();
     counter += 1;
     contEl.innerHTML = `${counter}`;
+    checkNumber();
 }
 
 buttonEl.addEventListener('click', buttonClickHandler);
