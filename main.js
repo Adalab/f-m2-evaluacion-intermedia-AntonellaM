6179234a6832acb47145557b0040a1a2e9fdb9e4
antonellam <antonella.michelli@gmail.com>
2019-04-09 23:29:04 +0200

    Erase comments

--- main.js
+++ main.js
@@ -2,23 +2,12 @@
 
 console.log('Ready to go');
 
-//Crear un número aleatorio entre 1 y 100 y guardarlo en una constante. Puede ser constante porque se generará cada vez que recarguemos la página
-
-function randomNum() {
-    return Math.ceil(Math.random() * 100);
-}
-
 const randomNumber = randomNum(100);
-console.log(randomNumber);
-
-//Guardar el elemento input que contiene el número de la usuaria, el botón, el párrafo y el contador.
-
 const numberEl = document.querySelector('.number__input');
 const buttonEl = document.querySelector('.number__submit');
 const clueEl =  document.querySelector('.clue');
 const contEl = document.querySelector('.counter');
 console.log(numberEl, buttonEl, clueEl, contEl);
-
 
 const counter = {
     initial: 0,
@@ -29,13 +18,9 @@ const counter = {
     },
 }
 
-
-//La función sobre el botón realizará varias acciones
-    //Comparar el número introducido por la usuaria con el número aleatorio generado
-        //Si el número es menor, imprimir un mensaje en pantalla: "Prueba un número más alto"
-        //Si el número es mayor, imprimir un mensaje en pantalla: "Prueba un número más bajo"
-        //Si acierta, imprimir un mensaje en pantalla: "ERES UNA CAMPEONA!"
-    //Accionar el contador y sumar uno cada vez que la usuaria pulse el botón
+function randomNum() {
+    return Math.ceil(Math.random() * 100);
+}
 
 function checkNumber () {
     let comparedNumber = parseInt(numberEl.value);
@@ -57,5 +42,4 @@ function buttonClickHandler(event) {
     contEl.innerHTML = `${counter.current}`;
 }
 
-//Poner un lístener sobre el botón
 buttonEl.addEventListener('click', buttonClickHandler);
